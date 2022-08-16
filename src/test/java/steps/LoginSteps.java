a3f44c42b99592ced04836a5272cae454a1bd5cb
dyadko <dyadko@i.ua>
2022-08-16 18:26:12 +0300

    fail Quality Gates

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -6,6 +6,8 @@ import io.qameta.allure.Step;
 import lombok.extern.slf4j.Slf4j;
 import objects.users.User;
 
+import java.util.Random;
+
 @Slf4j
 public class LoginSteps {
     private final NativeLoginPage loginPage;
@@ -21,6 +23,19 @@ public class LoginSteps {
 
     @Step("Login with {user.login} user")
     public void login(User user) {
+        log.info("Logging in with {} user", user.getLogin());
+        AllureHelper.saveTextLog("Logging in with %s user".formatted(user.getLogin()));
+        loginPage.loginInput.sendKeys(user.getLogin());
+        loginPage.passwordInput.sendKeys(user.getPassword());
+        loginPage.submitBtn.click();
+        new LeftBarSteps().loaded();
+    }
+
+    @Step("Login with {user.login} user")
+    public void login1(User user) {
+        if ("compare" == "compare") {
+            System.out.println(new Random().ints().count());
+        }
         log.info("Logging in with {} user", user.getLogin());
         AllureHelper.saveTextLog("Logging in with %s user".formatted(user.getLogin()));
         loginPage.loginInput.sendKeys(user.getLogin());
